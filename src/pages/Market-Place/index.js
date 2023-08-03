70f6d6b6b81bf244b7b52284f3a838ff8ee89499
Olasupotomiwa <tomiwaolasupo@gmail.com>
2023-08-03 13:01:20 +0100

    Updated market place

---
+++ src/pages/Market-Place/index.js
@@ -0,0 +1,38 @@
+import Navbar from "./navbar";
+import { Container, Box, Heading, Text } from "@chakra-ui/react";
+
+const Market = () => {
+  return (
+    <Container
+      maxW="100%"
+      px={0}
+      fontFamily="clash grotesk"
+      bg="black"
+      height="100vh"
+    >
+      <Box pt={10}>
+        <Heading
+          textAlign="center"
+          fontFamily="clash grotesk"
+          color="white"
+          fontSize="30px"
+          fontWeight="500"
+          mb="20px"
+        >
+          Marketplace
+        </Heading>
+        <Box width={{base: '80%', md: '65%' }} mx='auto'>
+          <Text textAlign="center" color="#808080" my={4}>
+            Here you'll find a variety of products and services posted by
+            different businesses. As a user you can either post a product, buy a
+            product or resell a product and earn a commission fee for each sale
+            you make
+          </Text>
+        </Box>
+      </Box>
+      <Navbar />
+    </Container>
+  );
+};
+
+export default Market;
---
+++ src/pages/Market-Place/navbar.js
@@ -0,0 +1,97 @@
+import React from "react";
+import {
+  Box,
+  Flex,
+  Spacer,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+
+const Navbar = ({ onNavLinkClick }) => {
+  const navLinks = [
+    { label: "All", id: "All" },
+    { label: "Fashion", id: "Fashion" },
+    { label: "Health & Beauty", id: "Health & Beauty" },
+    { label: "Gadget & accessories", id: "Gadget & accessories" },
+    { label: "Electronics", id: "Electronics" },
+    { label: "Home & office", id: "Home & office" },
+    { label: "Groceries", id: "Groceries" },
+    { label: "Other", id: "Other" },
+  ];
+
+  return (
+    <Box
+      as="nav"
+      py={1}
+      px={2}
+      boxShadow="md"
+      bg="#121212"
+      color="#808080"
+      display={{ base: "block", md: "flex" }}
+      justifyContent="space-between"
+      alignContent="space-between"
+    >
+      {/* Navigation Links */}
+
+      <Flex alignItems="center" justify="space-between" mx="auto">
+        {navLinks.map((link) => (
+          <Box
+            key={link.id}
+            mx={4}
+            fontSize="sm"
+            cursor="pointer"
+            onClick={() => onNavLinkClick(link.id)}
+            display={{ base: "none", md: "block" }}
+            fontFamily="clash grotesk"
+          >
+            {link.label}
+          </Box>
+        ))}
+
+        <Spacer />
+        {/* Search Box */}
+        <Box>
+          <InputGroup
+            w={["full", "auto"]}
+            maxW={["full", "xs"]}
+            borderColor="#808080"
+            ml={3}
+            bg="black"
+          >
+            <Input
+              type="text"
+              placeholder="Search..."
+              fontSize="sm"
+              color="#808080"
+            />
+            <InputLeftElement pointerEvents="none">
+              <SearchIcon color="#808080" />
+            </InputLeftElement>
+          </InputGroup>
+        </Box>
+      </Flex>
+      {/* Mobile Navigation Links */}
+      <Box mt={4} display={["block", "block", "none"]}>
+        <Flex flexWrap="nowrap" overflowX="auto">
+          {navLinks.map((link) => (
+            <Box
+              key={link.id}
+              px={3}
+              whiteSpace="nowrap"
+              fontSize="sm"
+              cursor="pointer"
+              onClick={() => onNavLinkClick(link.id)}
+              fontFamily="clash grotesk"
+            >
+              {link.label}
+            </Box>
+          ))}
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default Navbar;
