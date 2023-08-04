463d6f29849a501d5b9168f841e2f49a39e36307
Olasupotomiwa <tomiwaolasupo@gmail.com>
2023-08-04 17:47:19 +0100

    Fixed bugs

--- src/pages/Market-Place/Products.js
+++ src/pages/Market-Place/Products.js
@@ -1,13 +1,13 @@
 import {
     Container,
-  Text,
+    Text
  
 } from "@chakra-ui/react";
 
 const Products = () => {
   return(
     <Container maxW='100%' bg='#121212' height='100vh'>
-
+ <Text>Products</Text>
 
     </Container>
   )
