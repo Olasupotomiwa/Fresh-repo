29f86a0be86ee296246371b148f10e9634fd675b
Olasupo Tomiwa <tomiwaolasupo@gmail.com>
2023-09-06 18:50:06 +0100

    Fixed location array

--- src/pages/SignUp/LocationArray.js
+++ src/pages/SignUp/LocationArray.js
@@ -7,16 +7,79 @@ const locationData = [
       {
         state: "Lagos",
         cities: [
+          "Alimosho",
+          "Ajeromi-Ifelodun",
+          "Kosofe",
+          "Mushin",
+          "Oshodi-Isolo",
+          "Ojo",
+          "Ikorodu",
+          "Surulere",
+          "Agege",
+          "Ifako-Ijaiye",
+          "Somolu",
+          "Amuwo-Odofin",
           "Lagos Mainland",
-          "Victoria Island",
-          "Ikeja" /* Add more cities */,
+          "Ikeja",
+          "Eti-Osa",
+          "Badagry",
+          "Apapa",
+          "Lagos Island",
+          "Epe",
+          "Ibeju-Lekki",
         ],
       },
       {
         state: "Oyo",
-        cities: ["Ibadan", "Ogbomosho", "Iseyin" /* Add more cities */],
+        cities: [
+          "Akinyele",
+  "Afijio",
+  "Atiba",
+  "Atisbo",
+  "Egbeda",
+  "Ibadan North",
+  "Ibadan North-East",
+  "Ibadan North-West",
+  "Ibadan South-West",
+  "Ibadan South-East",
+  "Ibarapa Central",
+  "Ibarapa East",
+  "Ibarapa North",
+  "Ido",
+  "Irepo",
+  "Iseyin",
+  "Itesiwaju",
+  "Iwajowa",
+  "Kajola",
+  "Lagelu",
+  "Ogbomosho North",
+  "Ogbomosho South",
+  "Oyo East",
+  "Oyo West",
+  "Olorunsogo",
+  "Oluyole",
+  "Ogo Oluwa",
+  "Orelope",
+  "Ori Ire",
+  "Ona Ara",
+  "Saki West",
+  "Saki East",
+  "Surulere",
+        ],
       },
-      // Add more states and their cities within Nigeria
+      // You can update or add more states within Nigeria as needed
+      {
+        state: "Abuja",
+        cities: [
+          "Central Area",
+          "Garki",
+          "Wuse",
+          // Add more cities in Abuja
+          "Kubwa",
+          "Asokoro",
+          // Add more cities as needed
+        ],
+      },
     ],
   },
   {
@@ -27,17 +90,41 @@ const locationData = [
         cities: [
           "Nairobi Central",
           "Kilimani",
-          "Westlands" /* Add more cities */,
+          "Westlands",
+          // Add more cities in Nairobi
+          "Lang'ata",
+          "Kasarani",
+          // Add more cities as needed
         ],
       },
       {
         state: "Mombasa",
-        cities: ["Mombasa Island", "Nyali", "Bamburi" /* Add more cities */],
+        cities: [
+          "Mombasa Island",
+          "Nyali",
+          "Bamburi",
+          // Add more cities in Mombasa
+          "Likoni",
+          "Kisauni",
+          // Add more cities as needed
+        ],
       },
-      // Add more states and their cities within Kenya
+      // You can update or add more states within Kenya as needed
+      {
+        state: "Kisumu",
+        cities: [
+          "Kisumu Central",
+          "Nyalenda",
+          "Mamboleo",
+          // Add more cities in Kisumu
+          "Manyatta",
+          "Kondele",
+          // Add more cities as needed
+        ],
+      },
     ],
   },
-  // Add more countries and their states and cities
+  // You can add more countries and their states and cities as needed
 ];
 
 export default locationData;
